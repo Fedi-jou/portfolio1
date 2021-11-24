ff9ed6da6dce52a0c5ce1de3732cf7ce0ebb61b2
Your Name <you@example.com>
2021-11-24 01:04:19 +0100

    add data

--- src/utiles/resumeData.js
+++ src/utiles/resumeData.js
@@ -182,6 +182,22 @@ const data = {
     {
       tag: "Reactjs",
       image:
+        "https://res.cloudinary.com/dc83alvmn/image/upload/v1637712040/SC15_ltae1z.png",
+      title: "Crypto currency tracker API",
+      caption: "click to see website and code",
+      description: "Visit website",
+      netlify: "https://currency-finance.netlify.app/",
+      links: [
+        {
+          link: "https://github.com/Fedi-jou?tab=repositories",
+          icon: <GitHubIcon />,
+        },
+      ],
+    },
+    //
+    {
+      tag: "Reactjs",
+      image:
         "https://res.cloudinary.com/dc83alvmn/image/upload/v1637677264/SC14_zd68h0.png",
       title: "Private chat room",
       caption: "click to see website and code",
